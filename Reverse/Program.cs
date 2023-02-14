dbc881ef17297d9afd871a4b41a32b1a373658b4
malavika.sunil <malavika.sunil@MALAVIKAKSUNIL>
2023-02-14 16:17:30 +0530

    Add project files.

---
+++ Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "MALAVIKA";
+            char[] charArray = new char[input.Length];
+            int j = input.Length - 1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                charArray[i] = input[j];
+                j--;
+            }
+
+            string reversed = new string(charArray);
+            Console.WriteLine(reversed);
+        }
+    }
+}
+ 
